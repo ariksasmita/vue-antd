e7d90f6f2c29e4e5b61b37baafd2e26fe742026c
Arik Sasmita <ariksasmita@gmail.com>
2022-07-17 11:17:56 +0800

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <a-button>
+      <template #icon><SearchOutlined /></template>
+    </a-button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import { Button } from 'ant-design-vue';
+import { SearchOutlined } from '@ant-design/icons-vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    AButton: Button,
+    SearchOutlined,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
