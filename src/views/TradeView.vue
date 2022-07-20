a261b0aa25816b278cc82bc83dadfd7ed0d580d1
Arik Sasmita <ariksasmita@gmail.com>
2022-07-20 20:39:36 +0800

    feat: chore add form item

--- src/views/TradeView.vue
+++ src/views/TradeView.vue
@@ -2,19 +2,41 @@
 	<layout>
 		<layout-content>
 			<a-button>Test Button</a-button>
+			<a-form :colon="false" label-align="left">
+				<a-form-item label="Trade Name">
+					<a-input v-model:value="formState.tradeName"></a-input>
+				</a-form-item>
+			</a-form>
 		</layout-content>
 	</layout>
 </template>
 
 <script>
-import { Button, Layout, LayoutContent } from 'ant-design-vue';
+import {
+	Button,
+	Layout,
+	LayoutContent,
+	Form,
+	FormItem,
+	Input
+} from 'ant-design-vue';
 
 export default {
   name: 'HomeView',
 	components: {
 		AButton: Button,
+		AForm: Form,
+		AFormItem: FormItem,
+		AInput: Input,
 		Layout,
 		LayoutContent
+	},
+	data () {
+		return {
+			formState: {
+				tradeName: ''
+			},
+		}
 	}
 }
 </script>
